982552341f31ac23b0a6ed1f3c24dd0a502fcc9e
Devin Ryan <devin.ryan514@gmail.com>
2018-06-25 03:31:15 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,7 +9,11 @@ function getIt(){
 
 function frameIt(){
   $('img').on('load', function(){
+<<<<<<< HEAD
     $('img').addClass('tasty')
+=======
+    $('img').className = 'tasty'
+>>>>>>> 6a5d71bb227665c0b08fcf8d6d83739de594fb75
     return;
   });
   return;
